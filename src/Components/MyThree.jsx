3f24643ca03cec6653d5d029a585b9c8aa5850c3
Andrew Stanecek <andstanecek@gmail.com>
2024-04-03 22:00:21 -0600

    Three model work in progress

--- src/Components/MyThree.jsx
+++ src/Components/MyThree.jsx
@@ -4,6 +4,7 @@ import { useEffect, useRef } from "react";
 
 function MyThree() {
   const refContainer = useRef(null);
+
   useEffect(() => {
     // === THREE.JS CODE START ===
     var scene = new THREE.Scene();
@@ -13,19 +14,38 @@ function MyThree() {
     // document.body.appendChild( renderer.domElement );
     // use ref as a mount point of the Three.js scene instead of the document.body
     refContainer.current && refContainer.current.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+    var geometry = new THREE.TorusGeometry(3, 1, 100, 100);
+
+    const texture = new THREE.TextureLoader().load('../images/NameTexture', (texture) => {
+      texture.minFilter = THREE.NearestFilter
+    })
+
+    var material = new THREE.ShaderMaterial({
+      // vertexShader, 
+      // fragmentShader, 
+      uniforms : {
+        uTime: { value: 0},
+        uTexture:  {value: texture}
+      },
+      transparent: true,
+      side: THREE.DoubleSide,
+    })
+    
+    // var material = new THREE.MeshNormalMaterial();
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
-    camera.position.z = 5;
+    camera.position.z = 15;
     var animate = function () {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
+      // cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
       renderer.render(scene, camera);
     };
     animate();
   }, []);
+
+
   return (
     <div ref={refContainer}></div>
 
